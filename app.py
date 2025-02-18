37e7dba3fa1497bd1059131ac7a8772ae00c6345
mayamorad <maya.z.morad@gmail.com>
2025-02-17 22:00:44 -0600

    edits

--- app.py
+++ app.py
@@ -1,30 +1,132 @@
-# Import the dependencies.
+from flask import Flask, jsonify
 
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-#################################################
-# Database Setup
-#################################################
+app = Flask(__name__)
 
+# connect to the database
+# connect engine to hawaii.sqlite
+engine = create_engine ("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
+Base.prepare(engine, reflect=True)
 
+# save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-# Save references to each table
+# create out session (link) from Python to the DB
+session = Session(engine)
 
+# home route
+@app.route("/")
+def home():
+    return(
+        f"<center><h2>Welcome to the Hawaii Climate Analysis Local API!</h2></center>"
+        f"<center><h3>Select from one of the available routes</h3></center>"
+        f"<center>/api/v1.0/precipitation</center>"
+        f"<center>/api/v1.0/stations</center>"
+        f"<center>/api/v1.0/tobs</center>"
+        f"<center>/api/v1.0/start/end</center>"
+    )
 
-# Create our session (link) from Python to the DB
+# /api/v1.0/precipitation route
+@app.route("/api/v1.0/precipitation")
+def precip():
+    # return the previous year's precipitation as a json
+    # Calculate the date one year from teh last date in data set
+    previousYear = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    #previousYear
 
+    # Perform a query to retrieve the date and precipitation scores
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= previousYear).all()
 
-#################################################
-# Flask Setup
-#################################################
+    session.close()
+    #dictionary with the date as the key and the precipitation (prcp) as the value
+    precipitation = {date: prcp for date, prcp in results}
+    # convert to a json
+    return jsonify(precipitation)
 
+# /api/v1.0/stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # show a list of stations
+    # Perform a query to retrieve the date and precipitation scores
+    results = session.query(Station.station).all()
+    session.close()
 
+    stationList = list(np.ravel(results))
 
+    # convert to a json and display
+    return jsonify(stationList)
 
-#################################################
-# Flask Routes
-#################################################
+# /api/v1.0/tobs route
+@app.route("/api/v1.0/tobs")
+def temperatures():
+    # return the previous year temperatures
+    # Calculate the date one year from the last date in the data set
+    previousYear = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    #previousYear
+
+    # Perform a query to retrieve the temeperatures from the most active station from the past year
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= previousYear).all()
+    session.close()
+
+    temperatureList = list(np.ravel(results))
+
+    #return the list of temperatures 
+    return jsonify(temperatureList)
+
+# /api/v1.0/start/end and /api/v1.0/start routes
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def dateStats(start=None, end=None):
+    
+    # select statement
+    selection = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
+
+    if not end:
+        
+        startDate = dt.datetime.strptime(start, "%m%d%Y")
+
+        results = session.query(*selection).filter(Measurement.date >= startDate).all()
+
+        session.close()
+        
+        temperatureList = list(np.ravel(results))
+
+        # return the list of temperatures
+        return jsonify(temperatureList)
+    
+    
+    else:
+        startDate = dt.datetime.strptime(start, "%m%d%Y")
+        endDate = dt.datetime.strptime(end, "%m%d%Y")
+
+        results = session.query(*selection)\
+            .filter(Measurement.date >= startDate)\
+            .filter(Measurement.date <= endDate).all()
+
+        session.close()
+        
+        temperatureList = list(np.ravel(results))
+
+        # return the list of temperatures
+        return jsonify(temperatureList)
+
+## app launcher
+if __name__ == '__main__':
+    app.run(debug=True)
